72d2d523f4cf964be0592993ddfa784b84aaaf7f
Pierre-Angelo <papeyrie@gmail.com>
2024-01-26 09:41:59 +0100

    objet joueur

---
+++ Projet_JS/Joueur.js
@@ -0,0 +1,26 @@
+function cree_joueur(){
+    joueur = {
+        tapis : [], // contient des mots
+        lettres : [], //contient des caractères
+        aff_tapis : function(){
+            res = "     9 16 25 36 49 64 81\n"
+            for(mot of this.tapis){
+                for (lettre of mot){
+                    res += " " + lettre 
+                }
+                res += "\n"
+            }
+            console.log(res)
+        }
+    
+    }
+
+    return joueur
+}
+
+ja = cree_joueur()
+ja.tapis=["bonjour","aurevoire","salut"]
+ja.aff_tapis()
+    
+
+
